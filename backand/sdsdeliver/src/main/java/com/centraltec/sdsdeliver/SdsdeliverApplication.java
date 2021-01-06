fc7f992da00c3dcd234b4766aac1a57b05fb09ed
EdsonTomas <tomasedson1@hotmail.com>
2021-01-06 22:37:21 +0000

    project created

---
+++ backand/sdsdeliver/src/main/java/com/centraltec/sdsdeliver/SdsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.centraltec.sdsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsdeliverApplication.class, args);
+	}
+
+}
---
+++ backand/sdsdeliver/src/test/java/com/centraltec/sdsdeliver/SdsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centraltec.sdsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
